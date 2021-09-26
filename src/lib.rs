7b67cbe082d46c89515588b70e3df02bfa835c5a
hjiayz <hjiayz@gmail.com>
2021-09-26 16:59:43 +0800

    0.2

--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,29 @@
-#![recursion_limit="256"]
+#![recursion_limit = "256"]
 
 //! cpp std::regex
 //! test only
 //! ```
-//!     use cpp_regexp::RegExp;
-//!     assert!(RegExp::new("^hello$").test("hello"));
-//!     let mut regex = RegExp::new("^hello$");
-//!     regex.icase = true;
+//!     use cpp_regexp::{RegExp,Config};
+//!     assert!(RegExp::new("^hello$",Default::default()).test("hello"));
+//!     let config = Config{
+//!         icase:true,
+//!         ..Default::default()
+//!     };
+//!     let mut regex = RegExp::new("^hello$",config);
 //!     assert!(regex.test("hellO"));
 //! ```
 
-use cpp::cpp;
+use cpp::{cpp, cpp_class};
+use std::marker::PhantomData;
 
-cpp!{{
+cpp! {{
     #include <iostream>
     #include <string>
     #include <regex>
 }}
 
-#[derive(Copy,Clone)]
-pub enum Grammar{
+#[derive(Copy, Clone)]
+pub enum Grammar {
     ECMAScript = 0,
     BasicPOSIX = 1,
     ExtendedPOSIX = 2,
@@ -28,44 +32,54 @@ pub enum Grammar{
     Egrep = 5,
 }
 
-pub struct RegExp<'a>{
-    exp:&'a str,
-    pub icase:bool,
-    pub nosubs:bool,
-    pub optimize:bool,
-    pub collate:bool,
-    pub kind:Grammar,
+#[derive(Copy, Clone)]
+pub struct Config {
+    pub icase: bool,
+    pub nosubs: bool,
+    pub optimize: bool,
+    pub collate: bool,
+    pub kind: Grammar,
 }
 
-impl<'a> RegExp<'a>{
-    pub fn new(exp:&'a str)->RegExp<'a>{
-        RegExp{
-            exp,
-            icase:false,
-            nosubs:false,
-            optimize:false,
-            collate:false,
-            kind:Grammar::ECMAScript,
+impl Default for Config {
+    #[inline]
+    fn default() -> Config {
+        Config {
+            icase: false,
+            nosubs: false,
+            optimize: false,
+            collate: false,
+            kind: Grammar::ECMAScript,
         }
     }
-    pub fn test(&self,s:&str)->bool{
-        let regexp = std::ffi::CString::new(self.exp).unwrap();
-        let regexp_ptr = regexp.as_ptr();
-        let s = std::ffi::CString::new(s).unwrap();
-        let s_ptr = s.as_ptr();
-        let kind:usize = self.kind as usize;
-        let icase = self.icase;
-        let nosubs = self.nosubs;
-        let optimize = self.optimize;
-        let collate = self.collate;
-        unsafe {
+}
+
+cpp_class! {
+    unsafe struct Regex as "std::regex"
+}
+
+pub struct RegExp<'a> {
+    exp: PhantomData<&'a ()>,
+    regexp: Regex,
+}
+
+impl<'a> RegExp<'a> {
+    #[inline]
+    pub fn new(exp: &'a str, config: Config) -> RegExp<'a> {
+        let regexp_str = std::ffi::CString::new(exp).unwrap();
+        let regexp_ptr = regexp_str.as_ptr();
+        let kind: usize = config.kind as usize;
+        let icase = config.icase;
+        let nosubs = config.nosubs;
+        let optimize = config.optimize;
+        let collate = config.collate;
+        let regexp = unsafe {
             cpp!([regexp_ptr as "const char *",
-                    s_ptr as "const char *",
                     icase as "bool",
-                    nosubs as "bool", 
-                    optimize as "bool", 
-                    collate as "bool", 
-                    kind as "size_t"] -> bool as "bool" {
+                    nosubs as "bool",
+                    optimize as "bool",
+                    collate as "bool",
+                    kind as "size_t"] -> Regex as "std::regex" {
                 auto flag = std::regex::ECMAScript;
                 switch(kind){
                     case 0:
@@ -98,8 +112,23 @@ impl<'a> RegExp<'a>{
                 if (collate) {
                     flag = flag | std::regex::collate;
                 }
-                std::regex regex(regexp_ptr,flag);
-                return std::regex_search(s_ptr,regex);
+                return std::regex(regexp_ptr,flag);
+            })
+        };
+        RegExp {
+            exp: PhantomData,
+            regexp,
+        }
+    }
+    #[inline]
+    pub fn test(&self, s: &str) -> bool {
+        let s = std::ffi::CString::new(s).unwrap();
+        let s_ptr = s.as_ptr();
+        let regexp = &self.regexp;
+        unsafe {
+            cpp!([  s_ptr as "const char *",
+                    regexp as "const std::regex *"] -> bool as "bool" {
+                return std::regex_search(s_ptr,*regexp);
             })
         }
     }
