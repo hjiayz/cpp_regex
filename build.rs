d6b675439691d640d16ba5d9a356d4a607a1f027
hjiayz <hjiayz@gmail.com>
2021-10-01 17:03:32 +0800

    fix to c++14

--- build.rs
+++ build.rs
@@ -1,7 +1,7 @@
 fn main() {
     cxx_build::bridge("src/lib.rs") // returns a cc::Build
         .file("src/regex.cc")
-        .flag_if_supported("-std=c++11")
+        .flag_if_supported("-std=c++14")
         .compile("cpp_regexp");
 
     println!("cargo:rerun-if-changed=src/lib.rs");
